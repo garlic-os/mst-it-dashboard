e6e4c512bd66e47d73aea7431e8af24af73b1f0d
Nate <14845347+the-garlic-os@users.noreply.github.com>
2021-12-10 11:48:08 -0600

    Exit on error

--- Update site.bat
+++ Update site.bat
@@ -4,10 +4,10 @@ echo Updating IT Dashboard to the latest version...
 
 @echo on
 set PATH=%PATH%;C:\Program Files\Git\bin
-git checkout previous-version
-git pull origin main
-git checkout main
-git pull origin develop
+git checkout previous-version || goto :error
+git pull origin main || goto :error
+git checkout main || goto :error
+git pull origin develop || goto :error
 @echo off
 
 echo.
@@ -16,3 +16,12 @@ echo If anything catches on fire, run "Revert update.bat".
 echo.
 echo Press any key to exit...
 pause > nul
+goto :EOF
+
+:error
+echo .
+echo An error occurred.
+echo Press any key to exit...
+pause > nul
+
+:EOF
