7aa7e094280496c43fced333ef12c840522da164
Brewer <gab89r@umsystem.edu>
2024-12-19 16:31:33 -0600

    Save Update_site.bat before switching branches

---
+++ Update_site.bat
@@ -0,0 +1,29 @@
+@echo off
+
+echo Updating IT Dashboard to the latest version...
+
+@echo on
+set PATH=%PATH%;C:\Program Files\Git\bin
+git checkout previous-version || goto :error
+git pull origin main || goto :error
+git checkout main || goto :error
+git pull origin develop || goto :error
+@echo off
+
+echo.
+echo Done.
+echo If anything catches on fire, run "Revert update.bat".
+echo.
+echo Press any key to exit...
+pause > nul
+goto :EOF
+
+:error
+@echo off
+echo.
+echo An error occurred.
+echo Press any key to exit...
+pause > nul
+goto :EOF
+
+:EOF
