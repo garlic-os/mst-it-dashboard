f98c242422b53aee7cf3a030d1a2eb4d4a9400ae
Brewer <gab89r@umsystem.edu>
2025-01-16 09:38:16 -0600

    Debug

--- scripts/dashboard/app.js
+++ scripts/dashboard/app.js
@@ -148,12 +148,15 @@ function handleAnalyzePhone() {
 function handleLaps(mode) {
 	var value = document.getElementById("lapsPassword").value;
 	var bitLocker = document.getElementById("bitLockerPassword").value;
+	
+	switch (mode) {
 		case 0: // LAPS
 			navigate("LAPS", "https://laps.mst.edu/auth-cgi-bin/cgiwrap/mstlaps/search.pl?query=" + value);
 			break;
 		case 1: // BitLocker
 			navigate("BitLocker", "https://bitlocker.mst.edu/auth-cgi-bin/cgiwrap/mstbitlocker/search.pl?query=" + bitLocker);
 			break;
+	}
 	clearInput("bitLockerPassword");
 	clearInput("lapsPassword");
 }
